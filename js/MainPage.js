286e270f780ab1ac0ae9cb5db1156a173d025845
sekinot <sekino@hutime.org>
2018-03-20 11:57:47 -0400

    initialize function

--- js/MainPage.js
+++ js/MainPage.js
@@ -1,3 +1,15 @@
+
+var hutime;
+var mainPanelCollection;
+
+function initialize () {
+    hutime = new HuTime("mainPanels");
+    mainPanelCollection = new HuTime.PanelCollection(document.getElementById("mainPanels").clientHeight);
+    mainPanelCollection.style.backgroundColor = "#cccccc";
+    hutime.appendPanelCollection(mainPanelCollection);
+    hutime.redraw();
+}
+
 function importLayer () {
     alert("ぼよん");
 }
