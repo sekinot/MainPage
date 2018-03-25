258378a65b96882d54ac86f92499f768da647160
sekinot <sekino@hutime.org>
2018-03-25 01:03:00 -0400

    Sample Data - initialized

--- js/MainPage.js
+++ js/MainPage.js
@@ -10,40 +10,6 @@ function initialize () {
     hutime.redraw();
 }
 
-// **** START sample data output ****
-
-var lyr0;
-var lyr1;
-var pnl0;
-function createSample () {
-    pnl0 = new HuTime.TilePanel(200);
-    mainPanelCollection.appendPanel(pnl0);
-
-
-    ///*
-    lyr0 = new HuTime.LineChartLayer(
-        new HuTime.CalendarChartRecordset("sample/kyoto.csv", "日付", "日付", "平均風速"));
-    // */
-    ///*
-    lyr1 = new HuTime.TLineLayer(
-        new HuTime.TLineRecordset("sample/gion.csv", "From", "To", "行事"));
-    // */
-
-    pnl0.appendLayer(lyr0);
-    pnl0.appendLayer(lyr1);
-    hutime.redraw(2457200.5, 2457238.5);
-    //lyr0.recordsets[0].useLoadedDataForJSON = true;
-    //lyr1.recordsets[0].useLoadedDataForJSON = true;
-
-}
-
-function displaySample () {
-    document.getElementById("sample").innerText =
-        JSON.stringify(pnl0);
-}
-
-// **** End sample data output ****
-
 function importLayer () {
     alert("ぼよん");
 }
