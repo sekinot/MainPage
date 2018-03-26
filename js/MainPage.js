28a8d23410ef9e8fa39bbd22bb6666e5ce65af49
sekinot <sekino@hutime.org>
2018-03-26 06:49:08 -0400

    Data import test

--- js/MainPage.js
+++ js/MainPage.js
@@ -48,3 +48,28 @@ function clickBranch (element) {
 
     return;
 }
+
+
+// **** Menu Operation ****
+function closeMenuItem (element) {
+    while (element.tagName.toLowerCase() == "ul" || element.tagName.toLowerCase() == "li") {
+        if (element.tagName.toLowerCase() == "li" &&
+            element.parentNode.parentNode.tagName.toLowerCase() == "li") {
+            element.parentNode.style.display = "none";
+            element.parentNode.style.display = "";
+        }
+        element = element.parentNode;
+    }
+}
+
+
+function importContainer (element) {
+    closeMenuItem(element);
+
+    var a =
+        HuTime.JSON.load("http://localhost:63342/WebHuTimeIDE/MainPage/html/sample/LineChartPanel.json",
+            function () {
+                mainPanelCollection.appendPanel(a.parsedObject);
+                hutime.redraw(2457200.5, 2457238.5);
+            });
+}
