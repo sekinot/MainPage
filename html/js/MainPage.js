f85ad6e64a4d3447504e43cb167918167d7d8e93
sekinot <sekino@hutime.org>
2020-11-10 20:28:37 +0900

    文字や図形のPreferences

--- html/js/MainPage.js
+++ html/js/MainPage.js
@@ -91,7 +91,7 @@ function initialize () {    // 全体の初期化
     importRemoteJsonContainer("http://localhost:63342/WebHuTimeIDE/MainPage/debug/sample/LineChartPanel.json");
 
 //    showDialog("dialogOLObject");
-//    dCrOLOOpen();
+//    dPOLOOpen("Shape");
 
 
 }
@@ -2937,13 +2937,14 @@ function dCrOLOOpen (type) {
     document.getElementById("dOLOApply").style.display = "none";
     document.getElementById("dOLOCreate").style.display = "block";
     document.getElementById("dOLOOk").style.display = "none";
+    dOLOSwitchXT ();
     switch (type) {
         case "Shape" :
             document.getElementById("dOLODialogTitle").innerText = "Create an On-Layer Shape";
             document.getElementById("dOLOShape").style.display = "block";
             document.getElementById("dOLOString").style.display = "none";
             document.getElementById("dOLOImage").style.display = "none";
-            dOLOSwitchXT ();
+            document.getElementById("dOLOShapeType").disabled = false;
             dOLOSwitchShapeType ();
             break;
 
@@ -3073,8 +3074,180 @@ function getTValue () {
     });
 }
 
-function dOLOApply () {
 
+function dPOLOOpen (type) {
+    let obj = document.getElementById("treeContextMenu").treeBranch.hutimeObject;
+    document.getElementById("dialogOLObject").type = type;
+    document.getElementById("dialogOLObject").operation = "Preference";
+    document.getElementById("dOLOApply").style.display = "block";
+    document.getElementById("dOLOCreate").style.display = "none";
+    document.getElementById("dOLOOk").style.display = "block";
+
+    document.getElementById("dOLOName").value = obj.name;
+    if (obj.position instanceof HuTime.RelativeXYPosition) {
+        document.getElementById("dOLOUseX").checked = false;
+        document.getElementById("dOLOUseT").checked = true;
+        document.getElementById("dOLOTCalendar").value = "101.1";
+        document.getElementById("dOLOXValue").value = obj.position.ofsX;
+        document.getElementById("dOLOYValue").value = obj.position.ofsY;
+        let time = HuTime.jdToTime(obj.position.position.t, 1);
+        document.getElementById("dOLOTValue").value =
+            time.year < 0 ? "-" : "" +
+            Math.abs(time.year) > 9999 ? Math.abs(time.year) :
+                ("000" + Math.abs(time.year).toString()).slice(-4) + "-" +
+                ("0" + time.month.toString()).slice(-2) + "-" +
+                ("0" + time.day.toString()).slice(-2);
+        //document.getElementById("dOLOTValue").value = obj.position.position.t;
+        document.getElementById("dOLOVValue").value = obj.position.position.v;
+    }
+    else {
+        document.getElementById("dOLOUseX").checked = true;
+        document.getElementById("dOLOUseT").checked = false;
+        document.getElementById("dOLOXValue").value = obj.position.x;
+        document.getElementById("dOLOYValue").value = obj.position.y;
+    }
+    dOLOSwitchXT ();
+
+    switch (type) {
+        case "Shape" :
+            document.getElementById("dOLODialogTitle").innerText =
+                "Preference of an On-Layer Shape";
+            document.getElementById("dOLOShape").style.display = "block";
+            document.getElementById("dOLOString").style.display = "none";
+            document.getElementById("dOLOImage").style.display = "none";
+
+            if (obj instanceof HuTime.Circle) {
+                document.getElementById("dOLOShapeType").value = "Circle";
+            }
+            else if (obj instanceof HuTime.Square) {
+                document.getElementById("dOLOShapeType").value = "Square";
+                document.getElementById("dOLOShapeRotate").value = obj.rotate;
+            }
+            else if (obj instanceof HuTime.Triangle) {
+                document.getElementById("dOLOShapeType").value = "Triangle";
+                document.getElementById("dOLOShapeRotate").value = obj.rotate;
+            }
+            else {
+                return;
+            }
+            document.getElementById("dOLOShapeSize").value = obj.width;
+            dOLOSwitchShapeType ();
+            document.getElementById("dOLOShapeType").disabled = true;   // 種類は変更不可
+            document.getElementById("dOLOFillColor").value = obj.style.fillColor;
+            document.getElementById("dOLOEdgeColor").value = obj.style.lineColor;
+            document.getElementById("dOLOEdgeWidth").value = obj.style.lineWidth;
+            break;
+
+        case "String" :
+            document.getElementById("dOLODialogTitle").innerText =
+                "Preference of an On-Layer String";
+            document.getElementById("dOLOShape").style.display = "none";
+            document.getElementById("dOLOString").style.display = "block";
+            document.getElementById("dOLOImage").style.display = "none";
+
+            document.getElementById("dOLOStringText").value = obj.text;
+            document.getElementById("dOLOStringFont").value = obj.style.fontFamily;
+            if (obj.style.fontStyle === "italic") {
+                if (obj.style.fontWeight === 700 || obj.style.fontWeight === "bold")
+                    document.getElementById("dOLOStringStyle").value = "italic bold";
+                else
+                    document.getElementById("dOLOStringStyle").value = "italic";
+            }
+            else {
+                if (obj.style.fontWeight === 700 || obj.style.fontWeight === "bold")
+                    document.getElementById("dOLOStringStyle").value = "bold";
+                else
+                    document.getElementById("dOLOStringStyle").value = "normal";
+            }
+            document.getElementById("dOLOStringColor").value = obj.style.fillColor;
+            document.getElementById("dOLOStringSize").value = parseFloat(obj.style.fontSize);
+            document.getElementById("dOLOStringRotate").value = obj.rotate;
+            break;
+
+        case "Image" :
+            document.getElementById("dOLODialogTitle").innerText =
+                "Preference of an On-Layer Image";
+            document.getElementById("dOLOShape").style.display = "none";
+            document.getElementById("dOLOString").style.display = "none";
+            document.getElementById("dOLOImage").style.display = "block";
+            //dOLOSwitchSourceLocationType();
+            document.getElementById("dOLOImageURL").value = obj.src;
+            document.getElementById("dOLOImageWidth").value = obj.width;
+            document.getElementById("dOLOImageHeight").value = obj.height;
+            document.getElementById("dOLOImageRotate").value = obj.rotate;
+            document.getElementById("dOLOImageEdgeColor").value = obj.style.lineColor
+            document.getElementById("dOLOImageEdgeWidth").value = obj.style.lineWidth;
+            break;
+    }
+    showDialog("dialogOLObject");
+}
+
+function dOLOApply () {
+    let obj = document.getElementById("treeContextMenu").treeBranch.hutimeObject;
+    switch (document.getElementById("dialogOLObject").type) {
+        case "Shape" :
+            if (obj instanceof HuTime.Square || obj instanceof HuTime.Triangle)
+                obj.rotate = parseFloat(document.getElementById("dOLOShapeRotate").value);
+            obj.width = parseFloat(document.getElementById("dOLOShapeSize").value);
+            obj.style = new HuTime.FigureStyle(
+                document.getElementById("dOLOFillColor").value,
+                document.getElementById("dOLOEdgeColor").value,
+                parseFloat(document.getElementById("dOLOEdgeWidth").value));
+            break;
+
+        case "String" :
+            obj.text = document.getElementById("dOLOStringText").value;
+            let style = new HuTime.StringStyle();
+            style.fontFamily = document.getElementById("dOLOStringFont").value;
+            let fontStyle = document.getElementById("dOLOStringStyle").value;
+            style.fontStyle = "normal";
+            style.fontWeight = 400;
+            if (fontStyle.indexOf("italic") >= 0)
+                style.fontStyle = "italic";
+            if (fontStyle.indexOf("bold") >= 0)
+                style.fontWeight = 700;
+            style.fontSize = document.getElementById("dOLOStringSize").value;
+            style.fillColor = document.getElementById("dOLOStringColor").value;
+            obj.style = style;
+            obj.rotate = parseFloat(document.getElementById("dOLOStringRotate").value);
+            break;
+
+        case "Image" :
+            obj.src = document.getElementById("dOLOImageURL").value;
+            obj.width = parseFloat(document.getElementById("dOLOImageWidth").value);
+            obj.height = parseFloat(document.getElementById("dOLOImageHeight").value);
+            obj.rotate = parseFloat(document.getElementById("dOLOImageRotate").value);
+            obj.style = new HuTime.FigureStyle(null,
+                document.getElementById("dOLOImageEdgeColor").value,
+                parseFloat(document.getElementById("dOLOImageEdgeWidth").value));
+            break;
+    }
+
+    if (document.getElementById("dOLOUseT").checked) {
+        getTValue().then(function (responseText) {
+            if (typeof (responseText) !== "string")
+                return;
+            obj.position = new HuTime.RelativeXYPosition(
+                new HuTime.TVPosition(
+                    parseFloat(responseText),
+                    parseFloat(document.getElementById("dOLOVValue").value)),
+                parseFloat(document.getElementById("dOLOXValue").value),
+                parseFloat(document.getElementById("dOLOYValue").value));
+            obj.parent.redraw();
+        }).catch(function (error) {
+            document.getElementById("statusBar").innerText =
+                "Error in converting T Value. (" + error.message + ")";
+        });
+    }
+    else {
+        obj.position = new HuTime.XYPosition(
+            parseFloat(document.getElementById("dOLOXValue").value),
+            parseFloat(document.getElementById("dOLOYValue").value));
+        obj.parent.redraw();
+    }
+    obj.name = document.getElementById("dOLOName").value;
+    document.getElementById("treeContextMenu").treeBranch.  // treeメニューのラベルを変更
+        querySelector("span.branchLabelSpan").innerText = obj.name;
 }
 function dOLOClose () {
     dOLOApply();
